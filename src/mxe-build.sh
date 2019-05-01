29848ba17648b4d317471c5f055bbdb54b60d34f
sibcool <sibcool@gmail.com>
2019-05-02 00:49:55 +0200

    Update mxe-build.sh

--- src/mxe-build.sh
+++ src/mxe-build.sh
@@ -80,13 +80,13 @@ sudo ./b2 toolset=gcc address-model=${ADDRESSMODEL} target-os=windows variant=re
 cd ..
 
 # Download, extract, build, install openssl1.0.2
-wget https://www.openssl.org/source/old/1.0.1/openssl-1.0.1f.tar.gz
-tar -xzvf openssl-1.0.1f.tar.gz > /dev/null
-cp -R openssl-1.0.1f openssl-win32-build
+wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2n.tar.gz
+tar -xzvf openssl-1.0.2n.tar.gz > /dev/null
+cp -R openssl-1.0.2n openssl-win32-build
 cd openssl-win32-build
 CROSS_COMPILE="${MXE_TARGET1}-" ./Configure mingw no-asm no-shared --prefix=$MXE_PATH/usr/${MXE_TARGET1}
-make 
-sudo make install 
+make > /dev/null
+sudo make install_sw
 cd ..
 
 # Download, extract, build, install BDB4.8.30
