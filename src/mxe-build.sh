a053b752259be2458f46b4afd0faf88150796133
sibcool <sibcool@gmail.com>
2019-05-02 18:38:40 +0200

    Update mxe-build.sh

--- src/mxe-build.sh
+++ src/mxe-build.sh
@@ -59,9 +59,7 @@ sudo apt-get --yes install mxe-${MXE_TARGET}-miniupnpc
 sudo apt-get --yes install mxe-${MXE_TARGET}-qttools
 sudo apt-get --yes -f install
 #sudo apt-get --yes install mxe-${MXE_TARGET}-db
-sudo apt-get install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
-sudo apt-get install libtool-bin
-apt-get install libgtk2.0-dev
+
 # Some variables used by both Qt and daemon builds.
 MXE_PATH=/usr/lib/mxe
 export PATH=$PATH:$MXE_PATH/usr/bin
