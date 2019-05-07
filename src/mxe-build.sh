c9c2b11fd45653e5a41f8c73834a33b078d60a00
sibcool <sibcool@gmail.com>
2019-05-07 16:16:23 +0200

    Update mxe-build.sh

--- src/mxe-build.sh
+++ src/mxe-build.sh
@@ -78,7 +78,7 @@ cd boost_1_63_0
 ./bootstrap.sh --without-icu
 echo "using gcc : mxe : $MXE_PATH/usr/bin/${MXE_TARGET1}-g++ : <rc>$MXE_PATH/usr/bin/${MXE_TARGET1}-windres <archiver>$MXE_PATH/usr/bin/${MXE_TARGET1}-ar <ranlib>$MXE_PATH/usr/bin/${MXE_TARGET1}-ranlib ;" > user-config.jam
 export PATH=/usr/lib/mxe/usr/bin:$PATH
-sudo ./b2 toolset=gcc address-model=${ADDRESSMODEL} target-os=windows variant=release threading=multi threadapi=pthread \
+sudo ./b2 toolset=gcc address-model=${ADDRESSMODEL} target-os=windows variant=release threading=multi threadapi=win32 \
 	link=static runtime-link=static --prefix=$MXE_PATH/usr/${MXE_TARGET1} --user-config=user-config.jam \
 	--without-mpi --without-python -sNO_BZIP2=1 --layout=tagged install
 cd ..
